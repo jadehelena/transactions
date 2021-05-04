e6b9b26d832ff9245204aa1b0854be2d5077a209
jadehelena <jademoraes@gmail.com>
2021-05-03 23:41:01 -0300

    :white_check_mark: Updated unit tests to work without a database

--- src/test/groovy/com/jadehelena/transactions/TransactionsApplicationTests.groovy
+++ src/test/groovy/com/jadehelena/transactions/TransactionsApplicationTests.groovy
@@ -3,10 +3,8 @@ package com.jadehelena.transactions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class TransactionsApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
